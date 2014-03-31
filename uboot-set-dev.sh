49bc6b20fa8e946d52390bc24aa9f221626440ad
Alan Xenos <alan.xenos@gmail.com>
2014-03-31 03:24:20 -0500

    Fixed  some bullshit got ready for Samsung and such

--- uboot-set-dev.sh
+++ uboot-set-dev.sh
@@ -13,29 +13,31 @@
 # the standard /dev/sda method of attaching the SSD/disk device. Samsung
 # devices require specific scripts which are not currently ready.
 
+#DEFINE VARS
 CHROME="chrome"
 UBUNTU="ubuntu"
 INSTALL="install"
 CROSINST="chromeinstall"
 HELP="help"
 DEV="developer"
+FUCKING_SAMSUNG_SHIT="sam"
 
-
+#MOVE HEADER BULLSHIT HERE TO AVOID MULTIPRINT KLUDGE
+echo "U-Boot Configuration Utility 1.22-DEV"
+echo "INTERNAL RELEASE ONLY"
+echo "========================================="
 
 if [ "$1" = $INSTALL ]
 then
-	echo "U-Boot Configuration Utility 1.2"
-	echo "INTERNAL RELEASE ONLY"
-	echo "========================================="
 	echo "Clearing old install"
-	if [ -f /usr/bin/uboot-set ];
+	if [ -a /usr/bin/uboot-set ];
 	then
 		cp /usr/bin/uboot-set /usr/bin/uboot.bkp
 		rm -rf /usr/bin/uboot-set
 	echo "Installing..."
 	sudo cp ./uboot-set-dev.sh /usr/bin/uboot-set
-	echo "Install Complete!"
 
+	echo "Install Complete!"
 elif [ "$1" = $CROSINST ]
 then
 	echo "U-Boot Configuration Utility 1.2"
@@ -46,19 +48,28 @@ then
 	
 elif [ "$1" = $UBUNTU ]
 then
-	echo "U-Boot Configuration Utility 1.2"
-	echo "INTERNAL RELEASE ONLY"
-	echo "========================================"
+	if [ -d /etc/fssind ];
+	then
+		sudo cgpt add <SAMSUNG SHIT PART>
+	elif
     sudo cgpt add -i 6 -P 5 -S 1 /dev/sda
     echo "Next time you reboot, you will boot into Ubuntu"
 
 elif [ "$1" = $CHROME ]	
 then
-	echo "U-Boot Configuration Utility 1.2"
-	echo "INTERNAL RELEASE ONLY"
-	echo "========================================"		
+	if [ -d /etc/fssind ];
+	then
+		sudo cgpt add <SAMSUNG SHIT PART>
+	elif
 	sudo cgpt add -i 6 -P 0 -S 1 /dev/sda
    	echo "Next time you reboot, you will boot into ChromeOS"
+	
+elif [ "$1" = $FUCKING_SAMSUNG_SHIT ]
+then
+	echo "You've specified Samsung-Compatible mode. One moment while we configure..."
+	mkdir /etc/fssind
+	echo "Please remember to run this in Ubuntu and Chrome OS BOTH!"
+	echo "From now on, U-Boot will operate in Samsung-Compatible mode."
 	
 
 elif [ "$1" = $HELP ]
@@ -81,9 +92,9 @@ then
         echo "[developer] to print developer build information"
 elif [ "$1" = $DEV ]
 then 
-        echo "Unique Build ID: Q8YTAHP98"
-        echo "Unique Build Phrase: Say my name, mister fahrenheit!"
-        echo "Version: 1.21 Date Of Release: 31.3.2014"
+        echo "Unique Build ID: AFI1309I"
+        echo "Unique Build Phrase: Sister moon shall be my guide"
+        echo "Version: 1.22 Date Of Release: 31.3.2014"
         echo "NOT WARRANTED FOR PUBLIC USE"
 else
 	echo "No valid flags specified. Try uboot-set help"
